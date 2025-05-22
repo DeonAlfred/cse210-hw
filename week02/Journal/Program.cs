b36124f9c1d965f0374c793c0361918c8646c590
DeonAlfred <deonjohannes1984@gmail.com>
2025-05-22 06:28:31 +0200

    commit Fractions

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -15,25 +15,23 @@ class Program
         Console.Write("Choose Your Number : ");
         string answer = Console.ReadLine();
         int ans = int.Parse(answer);
-        Entry entry2 = new Entry();
-        entry2._promptText =  "hello";
-        entry2.Display();
 
-    
+        Entry promp = new Entry();
+        Console.WriteLine(promp._promptText )
+
 
 
 
         if (ans == 1)
         {
             Console.WriteLine("You have chosen to Write in your Journal");
-            Console.WriteLine(_entryText);
-
 
         }
+
         else if (ans == 2)
         {
             Console.WriteLine("We are now viewing your latest entries");
-            Console.WriteLine(entry2);
+
         }
         else if (ans == 3)
         {
@@ -49,7 +47,7 @@ class Program
             Console.WriteLine("Thank You For Using Your Journal, Goodbye!");
         }
 
-       
+
     }
 
 }
